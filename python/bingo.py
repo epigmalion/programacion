2e21007715bc9437b847330be8675d80eb99dcca
epigmalion <epigmalion36@gmail.com>
2022-01-07 17:19:59 +0100

    primera version

---
+++ python/bingo.py
@@ -0,0 +1,16 @@
+import random
+
+numeros = []
+while len(numeros) < 90:
+    bola = random.randrange(1,91)
+    if bola in numeros:
+       continue
+    numeros.append(bola)
+    print ('El...', bola)
+print ('Extraccion finalidad, los números han sido...')
+print (numeros)
+print ('Un total de',len(numeros),'bolas')
+print ('Ordenando las bolas de nuevo...')
+numeros.sort()
+print (numeros)
+print ("That's all folks !")
