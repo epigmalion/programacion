346dc5a35a32d9c1485284dde09df83b94bb0522
epigmalion <epigmalion36@gmail.com>
2024-02-18 12:19:17 +0100

    fibonacci
    Primera versión de fibonacci

---
+++ python/fibonacci.py
@@ -0,0 +1,10 @@
+# A simple fibonnacci calculation algorithm
+def fibonacci(nthNumber):
+    a, b = 1, 1
+    print('a = %s, b = %s' % (a, b))
+    for i in range(2, nthNumber):
+        a, b = b, a + b # Get the next Fibonacci number.
+        print('a = %s, b = %s' % (a, b))
+    return a
+
+print(fibonacci(10))
