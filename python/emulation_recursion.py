856b3ee80da892530f78e2ff2c82e1be00a2cc96
epigmalion <epigmalion36@gmail.com>
2024-02-18 13:26:09 +0100

    Update emulation_recursion.py

--- python/emulation_recursion.py
+++ python/emulation_recursion.py
@@ -1,6 +1,7 @@
 callStack = [] # The explicit call stack, which holds "frame objects".
 callStack.append({'returnAddr': 'start', 'number': 5}) # "Call" the "factorial() function".
 returnValue = None
+print(callStack)
 
 while len(callStack) > 0:
     # The body of the "factorial() function":
@@ -13,16 +14,19 @@ while len(callStack) > 0:
             # BASE CASE
             returnValue = 1
             callStack.pop() # "Return" from "function call".
+            print(callStack,'end')
             continue
         else:
             # RECURSIVE CASE
             callStack[-1]['returnAddr'] = 'after recursive call'
             # "Call" the "factorial() function":
             callStack.append({'returnAddr': 'start', 'number': number - 1})
+            print(callStack,'end')
             continue
     elif returnAddr == 'after recursive call':
         returnValue = number * returnValue
         callStack.pop() # "Return from function call".
+        print(callStack,'end')
         continue
 
 print(returnValue)
