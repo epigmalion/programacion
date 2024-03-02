f8b3d84cb45f56c773d801282c92ba94ade7a551
epigmalion <epigmalion36@gmail.com>
2024-03-02 10:00:29 +0100

    bonito

--- python/bingo.py
+++ python/bingo.py
@@ -6,11 +6,11 @@ while len(numeros) < 90:
     if bola in numeros:
        continue
     numeros.append(bola)
-    print ('El...', bola)
-print ('Extraccion finalizada, los números han sido...')
-print (numeros)
-print ('Un total de',len(numeros),'bolas')
-print ('Ordenando las bolas de nuevo...')
+    print('El...', bola)
+print('Extraccion finalizada, los números han sido...')
+print(numeros)
+print('Un total de',len(numeros),'bolas')
+print('Ordenando las bolas de nuevo...')
 numeros.sort()
-print (numeros)
-print ("That's all folks !")
+print(numeros)
+print("That's all folks !")
